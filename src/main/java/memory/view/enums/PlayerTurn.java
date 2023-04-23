5beca9694828f6ff627725615b255d1431b33040
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-04-23 20:11:49 +0200

    final commit

---
+++ src/main/java/memory/view/enums/PlayerTurn.java
@@ -0,0 +1,9 @@
+package memory.view.enums;
+
+// Enumération listant les différents états possible des tours. Permet de déterminer à quel joueur est-ce le tour
+
+public enum PlayerTurn {
+	PLAYERONE,
+	PLAYERTWO,
+	NONE;
+}
