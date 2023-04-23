bf12b41aebdd09cb1cca1688baee8cf6ee763052
Antwan <94127380+Antwannnn@users.noreply.github.com>
2023-04-23 20:10:37 +0200

    Delete Memory directory

--- Memory/src/main/java/memory/view/enums/PlayerTurn.java
+++
@@ -1,9 +0,0 @@
-package memory.view.enums;
-
-// Enumération listant les différents états possible des tours. Permet de déterminer à quel joueur est-ce le tour
-
-public enum PlayerTurn {
-	PLAYERONE,
-	PLAYERTWO,
-	NONE;
-}
